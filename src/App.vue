b184b277aaaf03ba711cd112938e53f5e0bbed0a
WoShiGaoFuShuai <efremovmaxim95@mail.ru>
2022-09-14 12:27:47 +0300

    finished project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-app>
+    <v-main>
+      <Pomodoro-timer
+        @settingsModal="settingsModalToggle"
+        @sendTabs="recieveTabs"
+        :updatedTimers="updatedTimers"
+      />
+      <Settings-modal
+        :settingsModal="settingsModal"
+        @closeModal="closeModal"
+        :tabs="tabs"
+        @submitModal="modalSubmitted($event)"
+      />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import PomodoroTimer from "./components/Pomodoro-timer.vue";
+import SettingsModal from "./components/Settings-modal.vue";
 
 export default {
   name: "App",
+
   components: {
-    HelloWorld,
+    PomodoroTimer,
+    SettingsModal,
+  },
+
+  data() {
+    return {
+      settingsModal: false,
+      tabs: [
+        {
+          name: "Pomodoro",
+          icon: "mdi-timer-sand",
+          totalSeconds: 25 * 60,
+        },
+        {
+          name: "Short Break",
+          icon: "mdi-coffee-outline",
+          totalSeconds: 5 * 60,
+        },
+        {
+          name: "Long Break",
+          icon: "mdi-bed",
+          totalSeconds: 10 * 60,
+        },
+      ],
+      updatedTimers: {},
+    };
+  },
+  methods: {
+    settingsModalToggle() {
+      this.settingsModal = !this.settingsModal;
+    },
+    closeModal() {
+      this.settingsModal = false;
+    },
+    recieveTabs(recievedTabs) {
+      this.tabs = recievedTabs;
+    },
+    modalSubmitted(updatedTimers) {
+      this.updatedTimers = updatedTimers;
+    },
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss"></style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from "vue";
 import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import Vuetify from "vuetify/lib";
 
 Vue.config.productionTip = false;
 
 new Vue({
+  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  icons: {
+    iconfont: "fa", // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
+  },
+});
